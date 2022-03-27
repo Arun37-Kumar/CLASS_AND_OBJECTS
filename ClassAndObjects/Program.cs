5f92ca902b320957ae9fe1f3a3bdc9892aa20f04
Arun37-Kumar <arun007624@gmail.com>
2022-03-27 22:50:27 +0530

    commit made

---
+++ ClassAndObjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float AreaOfRectangle = overloading.area(5,5);
+            float AreaOfSquare = overloading.area(5);
+            Console.WriteLine(AreaOfSquare);
+            Console.WriteLine(AreaOfRectangle);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassAndObjects/overloading.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjects
+{
+    class overloading
+    {
+        public static float area(int x, int y)
+        {
+            return x * y;
+        }
+        public static int area(int x)
+        {
+            return x * x;
+        }
+    }
+}
